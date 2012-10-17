4a155d7577f77a44ac1535512cbdb48a461607bf
Guy Wrigley <gejaheline@googlemail.com>
2012-10-17 21:16:31 +0100

    WIP

--- players/guy_player.rb
+++ players/guy_player.rb
@@ -1,9 +1,9 @@
 class GuyPlayer
-  
+
   def name
     "Team Totally Super Special Awesome"
   end
-  
+
   def take_turn(state, colour)
     our_colour = colour
     if our_colour == :blue
@@ -24,9 +24,10 @@ class GuyPlayer
       end
       if counter == 3
         return column_no if is_legal_move? column_no
+      end
     end
-    
-    
+
+
     #check rows for three in a row of their colour
     0..6.each do |row|
       examined_columns = []
@@ -39,7 +40,7 @@ class GuyPlayer
           counter = 0
           examined_columns = []
         end
-        
+
         if counter == 3
           return (column_no + 1) if is_legal_move? (column_no + 1)
           return (examined_columns.first - 1) if is_legal_move? (examined_columns.first - 1)
@@ -59,63 +60,64 @@ class GuyPlayer
       end
       if counter == 3
         return column_no if is_legal_move? column_no
-    end
-    #check rows for three in a row of their colour
-    0..6.each do |row|
-      examined_columns = []
-      counter = 0
-      columns.each_with_index do |column,column_no|
-        examined_columns << column_no
-        if column[row] == their_colour
-          counter ++
-        else
-          counter = 0
-          examined_columns = []
-        end
-        
-        if counter == 3
-          return (column_no + 1) if is_legal_move? (column_no + 1)
-          return (examined_columns.first - 1) if is_legal_move? (examined_columns.first - 1)
+      end
+      #check rows for three in a row of their colour
+      0..6.each do |row|
+        examined_columns = []
+        counter = 0
+        columns.each_with_index do |column,column_no|
+          examined_columns << column_no
+          if column[row] == their_colour
+            counter ++
+          else
+            counter = 0
+            examined_columns = []
+          end
+
+          if counter == 3
+            return (column_no + 1) if is_legal_move? (column_no + 1)
+            return (examined_columns.first - 1) if is_legal_move? (examined_columns.first - 1)
+          end
         end
       end
     end
-        
-        
-    #Backup plan
-    random_number = rand(7)
-    while !is_legal_move?(random_number)
+
+
+      #Backup plan
       random_number = rand(7)
+      while !is_legal_move?(random_number)
+        random_number = rand(7)
+      end
+      return random_number
     end
-    return random_number
-  end
-  
-  def find_possible_moves(state)
-    legal_moves = []
-    state.each_with_index do |column, column_no|
-      if column.reverse.find :none
-        legal_moves << [column_no, column.reverse.find(:none)]
+
+    def find_possible_moves(state)
+      legal_moves = []
+      state.each_with_index do |column, column_no|
+        if column.reverse.find :none
+          legal_moves << [column_no, column.reverse.find(:none)]
+        end
+      end
+      return legal_moves
+    end
+
+    def find_legal_moves(state)
+      legal_moves = []
+      state.each_with_index do |column, column_no|
+        if column.reverse.find :none
+          legal_moves << column_no
+        end
+      end
+      return legal_moves
+    end
+
+    def is_legal_move?(column)
+      move = column
+      if find_legal_moves.include? move
+        return true
+      else
+        return false
       end
     end
-    return legal_moves
-  end
-  
-  def find_legal_moves(state)
-    legal_moves = []
-    state.each_with_index do |column, column_no|
-      if column.reverse.find :none
-        legal_moves << column_no
-      end
-    end
-    return legal_moves
-  end
-  
-  def is_legal_move?(column)
-    move = column
-    if find_legal_moves.include? move
-      return true
-    else
-      return false
-    end
-  end
-  
-  def
+
+    def
